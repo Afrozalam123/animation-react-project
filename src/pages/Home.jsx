63299abd8640fca22a499935d183a8ab8b51d6a3
Afrozalam123 <alamafroz70400@gmail.com>
2025-11-29 13:21:47 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+ import Video from '../component/Home/Video'
+ import HomeHeroText from "../component/Home/HomeHeroText"
+ import HomeBottomText from "../component/Home/HomeBottomText"
+const Home = () => {
+  return (
+    <>
+    <div className='h-screen w-screen fixed'>
+      <Video />
+    </div>
+
+    <div className='h-screen w-screen relative flex flex-col'>
+      <HomeHeroText />
+      <HomeBottomText />
+      </div>  
+    </>
+
+    
+    
+   
+  )
+}
+
+export default Home
+
+{/* <div className='h-screen w-screen relative'>
+      <HomeHeroText />
+      <HomeBottomText />
+      </div>   */}

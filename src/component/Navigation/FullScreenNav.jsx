50c32bcccfdaf13a9e7ecc8eb85e199101f2e53b
Afrozalam123 <alamafroz70400@gmail.com>
2025-12-05 11:15:26 +0530

    updte code

---
+++ src/component/Navigation/FullScreenNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div>
+      <div>
+        <h1>project</h1>
+        <div>
+          <h2>POUR TOUT</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
